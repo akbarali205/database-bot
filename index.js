ebd12fe33531628e89b747952c3f2e8634189a22
akbarEdcrm <168091320+akbarEdcrm@users.noreply.github.com>
2024-05-31 01:38:42 +0500

    fix problem

--- index.js
+++ index.js
@@ -2,23 +2,23 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = "7495174688:AAElcfwgxlkW_w0QgSSRuUAq-2dep8YjGvY";
 const bot = new TelegramBot(token, {polling: true});
 
-bot.on("photo", (msg) => {
+bot.on("photo", async (msg) => {
     const chatId = msg.chat.id;
     const fileLength = msg.photo.length;
     const fileId = msg.photo[fileLength - 1].file_id;
 
-    bot.getFile(fileId).then((file) => {
+   await bot.getFile(fileId).then((file) => {
         const url = `https://api.telegram.org/file/bot${token}/${file.file_path}`;
         console.log(url);
         return bot.sendMessage(chatId, url);
     })
 });
 
-bot.on("video", (msg) => {
+bot.on("video", async (msg) => {
     const chatId = msg.chat.id;
     const fileId = msg.video.file_id;
 
-    bot.getFile(fileId).then((file) => {
+   await bot.getFile(fileId).then((file) => {
         const url = `https://api.telegram.org/file/bot${token}/${file.file_path}`;
         console.log(url);
         return bot.sendMessage(chatId, url);
