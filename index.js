e9e874b66ce35c84b1130d38c482946d2777d90e
akbarEdcrm <168091320+akbarEdcrm@users.noreply.github.com>
2024-06-04 15:08:26 +0500

    new

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const TelegramBot = require('node-telegram-bot-api');
 const token = "7495174688:AAElcfwgxlkW_w0QgSSRuUAq-2dep8YjGvY";
 const bot = new TelegramBot(token, {polling: true});
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
 
 bot.on("photo", async (msg) => {
     const chatId = msg.chat.id;
