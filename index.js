7ea9b2fbade3e3f6cb316f2802ea43ca5e00da42
akbarEdcrm <168091320+akbarEdcrm@users.noreply.github.com>
2024-06-06 02:13:18 +0500

    hello

--- index.js
+++ index.js
@@ -1,38 +1,50 @@
+// const TelegramBot = require('node-telegram-bot-api');
+// const token = process.env.TELEGRAM_BOT_TOKEN;
+// const bot = new TelegramBot(token, {polling: true});
+
+// bot.on("photo", async (msg) => {
+//     const chatId = msg.chat.id;
+//     const fileLength = msg.photo.length;
+//     const fileId = msg.photo[fileLength - 1].file_id;
+
+//    await bot.getFile(fileId).then((file) => {
+//         const url = `https://api.telegram.org/file/bot${token}/${file.file_path}`;
+//         console.log(url);
+//         return bot.sendMessage(chatId, url);
+//     })
+// });
+
+// bot.on("video", async (msg) => {
+//     const chatId = msg.chat.id;
+//     const fileId = msg.video.file_id;
+
+//    await bot.getFile(fileId).then((file) => {
+//         const url = `https://api.telegram.org/file/bot${token}/${file.file_path}`;
+//         console.log(url);
+//         return bot.sendMessage(chatId, url);
+//     })
+// })
+
+
+
 const TelegramBot = require('node-telegram-bot-api');
-const token = "7495174688:AAElcfwgxlkW_w0QgSSRuUAq-2dep8YjGvY";
-const bot = new TelegramBot(token, {polling: true});
-const express = require('express');
-const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+// Bot tokenini o'zingizning tokeningiz bilan almashtiring
+const token = process.env.TELEGRAM_BOT_TOKEN || '7495174688:AAElcfwgxlkW_w0QgSSRuUAq-2dep8YjGvY';
+
+// Webhook URL
+const url = process.env.WEBHOOK_URL || 'https://database-bot.vercel.app/';
+
+// Botni webhook rejimida ishga tushirish
+const bot = new TelegramBot(token, { webHook: { port: process.env.PORT || 3000 } });
+
+// Webhook'ni sozlash
+bot.setWebHook(`${url}/bot${token}`);
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  const text = msg.text;
+
+  // Botga xabar kelganda javob qaytarish
+  bot.sendMessage(chatId, `Siz yozdingiz: ${text}`);
 });
-
-const port = process.env.PORT || 8000;
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
-
-
-bot.on("photo", async (msg) => {
-    const chatId = msg.chat.id;
-    const fileLength = msg.photo.length;
-    const fileId = msg.photo[fileLength - 1].file_id;
-
-   await bot.getFile(fileId).then((file) => {
-        const url = `https://api.telegram.org/file/bot${token}/${file.file_path}`;
-        console.log(url);
-        return bot.sendMessage(chatId, url);
-    })
-});
-
-bot.on("video", async (msg) => {
-    const chatId = msg.chat.id;
-    const fileId = msg.video.file_id;
-
-   await bot.getFile(fileId).then((file) => {
-        const url = `https://api.telegram.org/file/bot${token}/${file.file_path}`;
-        console.log(url);
-        return bot.sendMessage(chatId, url);
-    })
-})
