43a5a1af5e5c686ce63173f7d9aefcb14b2b0ff2
akbarEdcrm <168091320+akbarEdcrm@users.noreply.github.com>
2024-06-06 02:58:33 +0500

    fix

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ const app = express();
 app.use(express.json());
 
 // Botni webhook rejimida ishga tushiring
-const bot = new TelegramBot(token);
+const bot = new TelegramBot(token, { webHook: { port: process.env.PORT || 3000 } });
 bot.setWebHook(`${url}/bot${token}`);
 
 // Bot uchun endpoint sozlash
@@ -86,3 +86,26 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+bot.on("photo", async (msg) => {
+    const chatId = msg.chat.id;
+    const fileLength = msg.photo.length;
+    const fileId = msg.photo[fileLength - 1].file_id;
+
+   await bot.getFile(fileId).then((file) => {
+        const url = `https://api.telegram.org/file/bot${token}/${file.file_path}`;
+        console.log(url);
+        return bot.sendMessage(chatId, url);
+    })
+});
+
+bot.on("video", async (msg) => {
+    const chatId = msg.chat.id;
+    const fileId = msg.video.file_id;
+
+   await bot.getFile(fileId).then((file) => {
+        const url = `https://api.telegram.org/file/bot${token}/${file.file_path}`;
+        console.log(url);
+        return bot.sendMessage(chatId, url);
+    })
+})
